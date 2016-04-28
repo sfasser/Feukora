a67d7aa891b33fcfe61d4569175f8f05d88f3b78
Feukora <sandro-fasser@bluewin.ch>
2016-04-28 17:31:02 +0200

    28.04.2016

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -5,7 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Welt!!");
-		System.out.println("Sandro!!");
+		System.out.println("Sandro!!23");
 	}
 
 }
